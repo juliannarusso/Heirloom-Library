8495830301a806c5723fb71eb74ee512fdb2bcec
matt9527-marist <97668582+matt9527-marist@users.noreply.github.com>
2022-11-15 10:09:26 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -3,7 +3,7 @@
 <html lang="en"> 
     <head>
         <meta charset="utf-8">
-        <title> Admin View </title>
+        <title> Heirloom Orders </title>
         <link rel="stylesheet" href="styles.css">
     </head> 
     <body>
