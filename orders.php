c7d162f95eaa83ca1174960001e68c70a8d04fe1
matt9527-marist <97668582+matt9527-marist@users.noreply.github.com>
2022-11-16 13:37:37 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -3,24 +3,13 @@
 <html lang="en"> 
     <head>
         <meta charset="utf-8">
-        <title> Admin View </title>
+        <title> Orders View </title>
         <link rel="stylesheet" href="styles.css">
     </head> 
     <body>
-        <div class="fixed-header">
-            <div class="container">
-                <header>
-                    <div class="menu">
-                        <b class="homebutton"><a href="index.php"><u style="color: rgb(198, 32, 38);">Home</u></a></b>
-                        <ul>
-                            <li><a href=""><u>Sign In</u></a></li>
-                            <li><a href=""><u>My Cart</u></a></li>
-                        </ul>
-                    </div>
-                </header>
-            </div>
-            <hr>
-        </div>
+        <?php include "header.php";
+        define ("FILE_AUTHOR", "M. Ong");
+         ?>
 
         <div class="container">
     	    <main>
@@ -51,7 +40,7 @@ if ($r) {
     echo "
                         <table border=1>
                             <tr>
-                                <th>ID</th>
+                                <th>Book ID</th>
                                 <th>Customer ID</th>
                                 <th>Order Date</th>
                                 <th>Total Price</th>
@@ -75,20 +64,7 @@ if ($r) {
 ?>
             </div>
         </div>
-            <div class="fixed-footer">
-                        <hr>
-                <div class="footer">
-                    <small class="info"><a href=""><u style="color: rgb(172, 62, 91);">HeirloomLibSupport@gmail.com</u></a></small>
-                    <ul>
-                        <li><a href="admin.php"><small><u>Administrator</u></small></a></li>
-                        <li><a href="customers.php"><small><u>Customers</u></small></a></li>
-                        <li><a href="creatingfaqs.html"><small><u>FAQ</u></small></a></li>
-                        <li><a href="disclaimer.html"><small><u>Disclaimer</u></small></a></li>
-                        <li><a href=""><small><u>Translate</u></small></a></li>
-                        <li><a href=""><small>(C) M. Ong</small></a></li>
-                    </ul>
-                </div>    
-            </div>
+            <?php include "footer.php"; ?>
         </header>
     </main>
     </body>
