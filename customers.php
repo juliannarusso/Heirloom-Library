14f2ca015e6cdfda370d93b44e597975b8d3c926
matt9527-marist <97668582+matt9527-marist@users.noreply.github.com>
2022-11-16 13:56:59 -0500

    Updated and cleaned file
    I added your FILE_AUTHOR and included our footer.

--- customers.php
+++ customers.php
@@ -3,26 +3,12 @@
 <html lang="en"> 
     <head>
         <meta charset="utf-8">
-        <title> Admin View </title>
+        <title> Customers View </title>
         <link rel="stylesheet" href="styles.css">
     </head> 
     <body>
-        <div class="fixed-header">
-            <div class="container">
-                <header>
-                    <div class="menu">
-                        <b class="homebutton"><a href="index.php"><u style="color: rgb(198, 32, 38);">Home</u></a></b>
-                        <ul>
-                            <li><a href="products.php">PRODUCTS</a></li>
-                            <li><a href="sellers.php">SELLERS</a></li>
-                            <li><a href="orders.php">ORDERS</a></li>
-                            <li><a href=""><u>Sign In</u></a></li>
-                            <li><a href=""><u>My Cart</u></a></li>
-                        </ul>
-                    </div>
-                </header>
-            </div>
-            <hr>
+        <?php include "header.php"; 
+        define("FILE_AUTHOR", "J. Russo"); ?>
         </div>
 
         <div class="container">
@@ -74,20 +60,9 @@ if ($r) {
 
 ?>
             </div>
+        </div>
+        <?php include "footer.php"; ?>
         </header>
     </main>
-    <div class="fixed-footer">
-            <hr>
-    <div class="footer">
-        <small class="info"><a href=""><u style="color: rgb(172, 62, 91);">HeirloomLibSupport@gmail.com</u></a></small>
-        <ul>
-            <li><a href="creatingfaqs.html"><small><u>FAQ</u></small></a></li>
-            <li><a href="disclaimer.html"><small><u>Disclaimer</u></small></a></li>
-            <li><a href=""><small><u>Translate</u></small></a></li>
-            <li><a href=""><small>(C) J. Russo 2022</small></a></li>
-        </ul>
-        
-    </div>        
-        </div>
-</body>
+    </body>
 </html>
