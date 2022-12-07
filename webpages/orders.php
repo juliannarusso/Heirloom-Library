6aeca8e23ec71f6d9db3ee928cf58dc730aa4738
matt9527-marist <97668582+matt9527-marist@users.noreply.github.com>
2022-12-07 16:10:06 -0500

    Updated orders to align with sellers layout

--- webpages/orders.php
+++ webpages/orders.php
@@ -4,11 +4,38 @@
     <head>
         <meta charset="utf-8">
         <title> Orders View </title>
-        <link rel="stylesheet" href="orderstylesheet.css">
+        <link rel="stylesheet" href="styles.css">
+        <style>
+            .wrapText td {
+                word-wrap: break-word;
+            }
+
+            form, input[type=radio] {
+                border-radius: 1cm;
+                color:whitesmoke;
+                background-color: indianred;
+                justify-content: center;
+                margin: 0 auto;
+                font-weight: bold;
+                padding:6px;
+                align-items: center;
+                width: 20.5cm;
+            }
+
+            input[type='submit'] {
+                border-radius: 1cm;
+                border-color: crimson;
+                background-color: crimson;
+                font-weight: bolder;
+                color:whitesmoke;
+                margin-left: 10px;
+                padding-right: 15px;
+            }
+        </style>
     </head> 
     <body>
         <?php 
-        session_start();
+        include "usercheck.php";
         require "../connect_db.php";
         include "header.php";
         include "error_handler.php";
@@ -36,19 +63,16 @@
 
 <?php
 
-    if (ISSET($_POST['sort']) && !ISSET($_POST['sortby'])) {
-        $q = "SELECT * FROM t6_order ORDER BY " . $_POST['sort'];
-        displaytable($q);
-    } else if (ISSET($_POST['sort']) && ISSET($_POST['sortby'])) {
-        $q = "SELECT * FROM t6_order ORDER BY " . $_POST['sort'] . " " . $_POST['sortby'];
-        displaytable($q);
-    } 
+    #Form Data Checking
 
-    if ($_SERVER['REQUEST_METHOD'] == "GET") {
-        $q = "SELECT * FROM t6_order";
+    if (isset($_POST['sort'])) { $sortInput = " ORDER BY " . $_POST['sort']; } else { $sortInput = " ORDER BY book_id"; }
+    if (isset($_POST['direction'])) { $directionInput = " " . $_POST['direction']; } else { $directionInput = " ASC";}
+
+        $q = "SELECT * FROM t6_order" . $sortInput . $directionInput;
         $r = mysqli_query($dbc, $q);
         if ($r) {
 
+            echo "<div class='wrapText'>";
             echo "
                                 <table border=1>
                                     <tr>
@@ -70,35 +94,32 @@
             }
 
             echo "</table>";
+            echo "</div>";
         }
-    }
     
 
+        echo "</div>";
+        echo "</div>";
+        echo "</div>";
+        echo "</div>";
+        echo "</div>";
     
-
-    echo "<br><div class='sortoptions'>";
-    echo "<center><form action='' method='POST'>";
-    echo "<input type='submit' name='sort' value='Sort By'>";
-    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'book_id'>";
-    echo "<label>Book ID</label>";
-    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'cus_id'>";
-    echo "<label>Customer ID</label>";
-    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'order_date'>";
-    echo "<label>Order Date</label>";
-    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'total_price'>";
-    echo "<label>Total Price</label>";
-    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'qty'>";
-    echo "<label>Quantity</label>";
-    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'status'>";
-    echo "<label>Status</label>";
-    echo "</center>";
-    echo "<br><center>";
-    echo "<input class='sortradiobuttons' type = 'radio' name = 'sortby' value = 'ASC'>";
-    echo "<label>Ascending</label>";
-    echo "<input class='sortradiobuttons' type = 'radio' name = 'sortby' value = 'DESC'>";
-    echo "<label>Descending</label>";
-    echo "</form>";
-    echo "</div>";
+    
+        if ($isEmployeeAdmin) {
+            #HTML FORMS
+            echo "<br><br><br>";
+            echo "<form action='' method='POST'>";
+            echo "<input type = 'radio' name = 'sort' value = 'book_id' checked> Book ID";
+            if ($sortInput == " ORDER BY cus_id") { echo "<input type = 'radio' name = 'sort' value = 'cus_id' checked> Customer ID"; } else { echo "<input type = 'radio' name = 'sort' value = 'cus_id'> Customer ID";}
+            if ($sortInput == " ORDER BY order_date") { echo "<input type = 'radio' name = 'sort' value = 'order_date' checked> Order Date"; } else { echo "<input type = 'radio' name = 'sort' value = 'order_date'> Order Date";}
+            if ($sortInput == " ORDER BY total_price") { echo "<input type = 'radio' name = 'sort' value = 'total_price' checked> Total Price"; } else { echo "<input type = 'radio' name = 'sort' value = 'total_price'> Total Price";}
+            if ($sortInput == " ORDER BY qty") { echo "<input type = 'radio' name = 'sort' value = 'qty' checked> Quantity"; } else { echo "<input type = 'radio' name = 'sort' value = 'qty'> Quantity";}
+            if ($sortInput == " ORDER BY status") { echo "<input type = 'radio' name = 'sort' value = 'status' checked> Status"; } else { echo "<input type = 'radio' name = 'sort' value = 'status'> Status";}
+            echo "<br><input type='radio' name='direction' value='ASC' checked>	Ascending";
+            if ($directionInput == " DESC") { echo "<input type = 'radio' name = 'direction' value = 'DESC' checked> Descending"; } else { echo "<input type = 'radio' name = 'direction' value = 'DESC'> Descending";}
+            echo "<br><input type='submit' value='Sort It!' >";
+            echo "</form>";
+        }
 
 
     if (ISSET($_SESSION['active_user'])) {
@@ -117,35 +138,6 @@
                     echo "<br><h4 style = 'text-align: center; text-decoration: none;'><a href='addorder.php'><u style = 'color: rgb(198, 32, 38); font-size: 17px;'>Manually Add an Order</u></a></h4>";
                 }
             }
-        }
-    }
-
-    function displaytable ($q) {
-        require "../connect_db.php";
-        $r = mysqli_query($dbc, $q);
-        if ($r) {
-
-            echo "
-                                <table border=1>
-                                    <tr>
-                                        <th>Book ID</th>
-                                        <th>Customer ID</th>
-                                        <th>Order Date</th>
-                                        <th>Total Price</th>
-                                        <th>Quantity</th>
-                                        <th>Status</th>
-                                    </tr>
-                ";
-
-
-
-            while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
-                echo "<tr>" . "<td>" . $row[0] . "</td>" . "<td style = 'word-wrap: break-word;'>" . $row[1] . "</td>";
-                echo "<td>" . $row[2] . "</td>" . "<td>" . $row[3] . "</td>";
-                echo "<td style = 'word-wrap: break-word;'>" . $row[4] . "</td>" . "<td style = 'word-wrap: break-word;'>" . $row[5] . "</td>";
-            }
-
-            echo "</table>";
         }
     }
 
