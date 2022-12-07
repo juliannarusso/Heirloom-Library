7f22812098987f7d3be6cbca955c05263b728bb1
matt9527-marist <97668582+matt9527-marist@users.noreply.github.com>
2022-12-07 15:49:21 -0500

    Added sort functionality to orders and style sheet for orders

--- webpages/orders.php
+++ webpages/orders.php
@@ -4,14 +4,16 @@
     <head>
         <meta charset="utf-8">
         <title> Orders View </title>
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="orderstylesheet.css">
     </head> 
     <body>
         <?php 
         session_start();
+        require "../connect_db.php";
         include "header.php";
+        include "error_handler.php";
         define ("FILE_AUTHOR", "M. Ong");
-         ?>
+        ?>
 
         <div class="container">
     	    <main>
@@ -34,58 +36,118 @@
 
 <?php
 
-    require "../connect_db.php";
-    $q = "SELECT * FROM t6_order";
-    $r = mysqli_query($dbc, $q);
-    if ($r) {
+    if (ISSET($_POST['sort']) && !ISSET($_POST['sortby'])) {
+        $q = "SELECT * FROM t6_order ORDER BY " . $_POST['sort'];
+        displaytable($q);
+    } else if (ISSET($_POST['sort']) && ISSET($_POST['sortby'])) {
+        $q = "SELECT * FROM t6_order ORDER BY " . $_POST['sort'] . " " . $_POST['sortby'];
+        displaytable($q);
+    } 
 
-        echo "
-                            <table border=1>
-                                <tr>
-                                    <th>Book ID</th>
-                                    <th>Customer ID</th>
-                                    <th>Order Date</th>
-                                    <th>Total Price</th>
-                                    <th>Quantity</th>
-                                    <th>Status</th>
-                                </tr>
-            ";
+    if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        $q = "SELECT * FROM t6_order";
+        $r = mysqli_query($dbc, $q);
+        if ($r) {
+
+            echo "
+                                <table border=1>
+                                    <tr>
+                                        <th>Book ID</th>
+                                        <th>Customer ID</th>
+                                        <th>Order Date</th>
+                                        <th>Total Price</th>
+                                        <th>Quantity</th>
+                                        <th>Status</th>
+                                    </tr>
+                ";
 
 
 
-        while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
-            echo "<tr>" . "<td>" . $row[0] . "</td>" . "<td style = 'word-wrap: break-word;'>" . $row[1] . "</td>";
-            echo "<td>" . $row[2] . "</td>" . "<td>" . $row[3] . "</td>";
-            echo "<td style = 'word-wrap: break-word;'>" . $row[4] . "</td>" . "<td style = 'word-wrap: break-word;'>" . $row[5] . "</td>";
+            while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+                echo "<tr>" . "<td>" . $row[0] . "</td>" . "<td style = 'word-wrap: break-word;'>" . $row[1] . "</td>";
+                echo "<td>" . $row[2] . "</td>" . "<td>" . $row[3] . "</td>";
+                echo "<td style = 'word-wrap: break-word;'>" . $row[4] . "</td>" . "<td style = 'word-wrap: break-word;'>" . $row[5] . "</td>";
+            }
+
+            echo "</table>";
         }
+    }
+    
 
-        echo "</table>";
+    
 
-        // if (ISSET($_SESSION['login_status'])) {
-        //     $login_status = "LOGGED IN";
-        //     echo "<br><h4 style = 'text-align: center; text-decoration: none;'><a href='addorder.php'><u style = 'color: rgb(198, 32, 38); font-size: 17px;'>Manually Add an Order</u></a></h4>";
-        // }
+    echo "<br><div class='sortoptions'>";
+    echo "<center><form action='' method='POST'>";
+    echo "<input type='submit' name='sort' value='Sort By'>";
+    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'book_id'>";
+    echo "<label>Book ID</label>";
+    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'cus_id'>";
+    echo "<label>Customer ID</label>";
+    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'order_date'>";
+    echo "<label>Order Date</label>";
+    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'total_price'>";
+    echo "<label>Total Price</label>";
+    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'qty'>";
+    echo "<label>Quantity</label>";
+    echo "<input class='sortradiobuttons' type = 'radio' name = 'sort' value = 'status'>";
+    echo "<label>Status</label>";
+    echo "</center>";
+    echo "<br><center>";
+    echo "<input class='sortradiobuttons' type = 'radio' name = 'sortby' value = 'ASC'>";
+    echo "<label>Ascending</label>";
+    echo "<input class='sortradiobuttons' type = 'radio' name = 'sortby' value = 'DESC'>";
+    echo "<label>Descending</label>";
+    echo "</form>";
+    echo "</div>";
 
-        if (ISSET($_SESSION['active_user'])) {
-            $active_user = $_SESSION['active_user'];
-        }
-        if (ISSET($_SESSION['login_status'])) {
-            $login_status = "LOGGED IN";
 
-            $q = "SELECT user_rank FROM t6_user WHERE username = \"" . $active_user . "\"";
-            // echo $q;
-            $r = mysqli_query($dbc, $q);
+    if (ISSET($_SESSION['active_user'])) {
+        $active_user = $_SESSION['active_user'];
+    }
+    if (ISSET($_SESSION['login_status'])) {
+        $login_status = "LOGGED IN";
 
-            if ($r) {
-                while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
-                    if ($row[0] == "Admin" || $row[0] == "Employee") {
-                        echo "<br><h4 style = 'text-align: center; text-decoration: none;'><a href='addorder.php'><u style = 'color: rgb(198, 32, 38); font-size: 17px;'>Manually Add an Order</u></a></h4>";
-                    }
+        $q = "SELECT user_rank FROM t6_user WHERE username = \"" . $active_user . "\"";
+        // echo $q;
+        $r = mysqli_query($dbc, $q);
+
+        if ($r) {
+            while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+                if ($row[0] == "Admin" || $row[0] == "Employee") {
+                    echo "<br><h4 style = 'text-align: center; text-decoration: none;'><a href='addorder.php'><u style = 'color: rgb(198, 32, 38); font-size: 17px;'>Manually Add an Order</u></a></h4>";
                 }
             }
         }
-}
+    }
 
+    function displaytable ($q) {
+        require "../connect_db.php";
+        $r = mysqli_query($dbc, $q);
+        if ($r) {
+
+            echo "
+                                <table border=1>
+                                    <tr>
+                                        <th>Book ID</th>
+                                        <th>Customer ID</th>
+                                        <th>Order Date</th>
+                                        <th>Total Price</th>
+                                        <th>Quantity</th>
+                                        <th>Status</th>
+                                    </tr>
+                ";
+
+
+
+            while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+                echo "<tr>" . "<td>" . $row[0] . "</td>" . "<td style = 'word-wrap: break-word;'>" . $row[1] . "</td>";
+                echo "<td>" . $row[2] . "</td>" . "<td>" . $row[3] . "</td>";
+                echo "<td style = 'word-wrap: break-word;'>" . $row[4] . "</td>" . "<td style = 'word-wrap: break-word;'>" . $row[5] . "</td>";
+            }
+
+            echo "</table>";
+        }
+    }
 
 ?>
             </div>
